27bb4567434dc991886a3166ad33b4fad5a29858
Gilsabo <gilsala_88@hotmail.com>
2023-10-17 10:11:15 +0200

    Add nav

--- app/page.js
+++ app/page.js
@@ -1,3 +1,40 @@
+import styles from './page.module.css';
+
 export default function Home() {
-  return <main>tennisoverflow</main>;
+  return (
+    <main>
+      <header className={styles.header}>
+        <div className={styles.logoSection}>
+          <div>tennisoverflow - </div>
+          <span>- logo </span>
+        </div>
+        <nav>
+          <ul className={styles.navigation}>
+            <li>
+              <a href="/#">Home</a>
+            </li>
+            <li>
+              <a href="/#">Get started</a>
+            </li>
+            <li>
+              <a href="/#">Videos</a>
+            </li>
+            <li>
+              <a href="/#">Community</a>
+            </li>
+            <li>
+              <a href="/#">Partners</a>
+            </li>
+            <li>
+              <a href="/#">About us</a>
+            </li>
+          </ul>
+        </nav>
+        <div>
+          <button>Sign up</button>
+          <button>Log in</button>
+        </div>
+      </header>
+    </main>
+  );
 }
