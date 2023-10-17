c4fc4e954d91cf9aef46fb574c2cb31b74795524
Gilsabo <gilsala_88@hotmail.com>
2023-10-17 15:51:38 +0200

    Add dashboard page

---
+++ app/dashboard/page.js
@@ -0,0 +1,46 @@
+import styles from './dashBoard.module.css';
+
+export default function Dashboard() {
+  return (
+    <>
+      <div>Dashboard</div>
+      <header className={styles.header}>
+        <div className={styles.logoSection}>
+          <div>tennisoverflow - </div>
+          <span>- logo </span>
+        </div>
+        <nav>
+          <form action="/search" method="get">
+            <label>
+              Search:
+              <input name="search" placeholder="Enter your search..." />
+            </label>
+            <button type="button">Search</button>
+          </form>
+        </nav>
+        <a href="/">upload</a>
+        <a href="/">notification</a>
+        <button>log out</button>
+      </header>
+      <main className={styles.mainContainer}>
+        <sidebar>
+          <ul>
+            <li>
+              <a href="/">Profile</a>
+            </li>
+            <li>
+              <a href="/">My videos</a>
+            </li>
+            <li>
+              <a href="/">History</a>
+            </li>
+          </ul>
+        </sidebar>
+
+        <div>
+          <div></div>
+        </div>
+      </main>
+    </>
+  );
+}
