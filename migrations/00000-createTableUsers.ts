8a27d790acc79f2907c21fde5948cb84dd38c039
Gilsabo <gilsala_88@hotmail.com>
2023-10-20 08:47:29 +0200

    Create table users

---
+++ migrations/00000-createTableUsers.ts
@@ -0,0 +1,25 @@
+import { Sql } from 'postgres';
+
+export async function up(sql: Sql) {
+  await sql`
+  CREATE TABLE users(
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_name varchar(30) NOT NULL,
+    first_name varchar(30) NOT NULL,
+    last_name varchar(30) NOT NULL,
+    email varchar(30) NOT NULL UNIQUE,
+    password varchar(30) NOT NULL,
+    age integer NOT NULL,
+    profile_picture_url varchar(250),
+    years_experience integer NOT NULL,
+    dominant_hand varchar(30) NOT NULL,
+    description varchar(250) NOT NULL
+  )
+  `;
+}
+
+export async function down(sql: Sql) {
+  await sql`
+  DROP TABLE users
+`;
+}
