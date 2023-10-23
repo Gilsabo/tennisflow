cae47c69b0df72c45d415aab408abae0c676ad7b
Gilsabo <gilsala_88@hotmail.com>
2023-10-23 18:35:09 +0200

    Add functio create users

--- database/users.ts
+++ database/users.ts
@@ -3,6 +3,10 @@ import { cache } from 'react';
 import { User } from '../migrations/00000-createTableUsers';
 import { sql } from './connect';
 
+export type UserWithPasswordHash = User & {
+  passwordHash: string;
+};
+
 export const getUsers = cache(async () => {
   // return users;
   const users = await sql<User[]>`
@@ -10,3 +14,18 @@ export const getUsers = cache(async () => {
   `;
   return users;
 });
+
+export const createUser = cache(
+  async (userName: string, passwordHash: string) => {
+    const [user] = await sql<User[]>`
+    INSERT INTO users
+      (user_name, password_hash)
+    VALUES
+      (${userName}, ${passwordHash})
+    RETURNING
+      id,
+      userName
+    `;
+    return user;
+  },
+);
