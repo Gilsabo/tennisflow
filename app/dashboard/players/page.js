8d9c6b11a9ab8acc7cb7c6faaf36a530b0544973
Gilsabo <gilsala_88@hotmail.com>
2023-10-23 17:07:00 +0200

    Map through data users and render

--- app/dashboard/players/page.js
+++ app/dashboard/players/page.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import { getUsers } from '../../../database/users';
 
-export default function Players() {
-  return <div>Players</div>;
+export default async function Players() {
+  const users = await getUsers();
+  console.log(users);
+
+  return (
+    <div>
+      <div>Players</div>
+      <div>
+        {users.map((user) => {
+          return (
+            <div key={`div-players-${user.lastName}`}>
+              <div>{user.userName}</div>
+              <div> {user.age}</div>
+              <div> {user.dominantHand}</div>
+              <img
+                src={`../images/${user.profilePictureUrl}.jpeg`}
+                alt={`${user.firstName}${user.lastName}`}
+                width={100}
+                height={100}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
