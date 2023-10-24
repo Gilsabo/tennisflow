2b9809f96e61d9ba2a32457365e4f4c591061d8a
Gilsabo <gilsala_88@hotmail.com>
2023-10-24 18:34:52 +0200

    Add workflow like register but with login

---
+++ app/api/(auth)/login/route.ts
@@ -0,0 +1,70 @@
+import bcrypt from 'bcrypt';
+import { NextRequest, NextResponse } from 'next/server';
+import { z } from 'zod';
+import { getUserWithPasswordHashByUsername } from '../../../../database/users';
+
+const loginSchema = z.object({
+  userName: z.string().min(3),
+  password: z.string().min(3),
+});
+
+export type LoginResponseBodyPost =
+  | {
+      user: { userName: string };
+    }
+  | {
+      errors: {
+        message: string;
+      }[];
+    };
+
+export async function POST(
+  request: NextRequest,
+): Promise<NextResponse<LoginResponseBodyPost>> {
+  const body = await request.json();
+  const result = loginSchema.safeParse(body);
+
+  if (!result.success) {
+    return NextResponse.json(
+      { errors: result.error.issues },
+      {
+        status: 400,
+      },
+    );
+  }
+
+  const userWithPasswordHash = await getUserWithPasswordHashByUsername(
+    result.data.userName,
+  );
+  console.log('userrrrr', userWithPasswordHash);
+
+  if (!userWithPasswordHash) {
+    return NextResponse.json(
+      { errors: [{ message: 'username or password not valid' }] },
+      { status: 403 },
+    );
+  }
+
+  const isPasswordValid = await bcrypt.compare(
+    result.data.password,
+    userWithPasswordHash.passwordHash,
+  );
+
+  console.log(
+    'checkkk',
+    isPasswordValid,
+    result.data.password,
+    userWithPasswordHash.passwordHash,
+  );
+
+  if (!isPasswordValid) {
+    return NextResponse.json(
+      { errors: [{ message: 'username or password not valid' }] },
+      { status: 401 },
+    );
+  }
+
+  return NextResponse.json({
+    user: { userName: userWithPasswordHash.userName },
+  });
+}
