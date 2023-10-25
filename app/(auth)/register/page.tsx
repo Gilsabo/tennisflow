707c2c6d62eee32a38d254e545c52058548f11bd
Gilsabo <gilsala_88@hotmail.com>
2023-10-25 20:07:47 +0200

    Add redirect function when user has valid cookie

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,10 +1,24 @@
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { getValidSessionByToken } from '../../../database/sessions';
 import RegistrationForm from './RegistrationForm';
 
-export default function Register() {
+export default async function RegisterPage() {
+  // 1. Checking if the sessionToken cookie exists
+  const sessionTokenCookie = cookies().get('sessionToken');
+  // 2. Check if the sessionToken cookie is still valid
+  const session =
+    sessionTokenCookie &&
+    (await getValidSessionByToken(sessionTokenCookie.value));
+
+  // 3. If the sessionToken cookie is valid, redirect to home
+
+  if (session) redirect('/dashboard');
+  // 4. If the sessionToken cookie is invalid or doesn't exist, show the login form
+
   return (
-    <>
-      <div>Register</div>
+    <div>
       <RegistrationForm />
-    </>
+    </div>
   );
 }
