3e83ae2f9311d11d67f75bab1acb3f7992a4f91a
Gilsabo <gilsala_88@hotmail.com>
2023-10-26 08:29:59 +0200

    Remove iframe. Give errors

--- app/dashboard/video.js
+++ app/dashboard/video.js
@@ -1,13 +1,3 @@
 export default function Video() {
-  return (
-    <iframe
-      title="video tennis"
-      src="https://player.cloudinary.com/embed/?public_id=shoes_video&cloud_name=demo"
-      width="640"
-      height="360"
-      style={{ height: 'auto', width: '100%', aspectRatio: '16/9' }}
-      allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
-      sandbox
-    />
-  );
+  return <div>video</div>;
 }
