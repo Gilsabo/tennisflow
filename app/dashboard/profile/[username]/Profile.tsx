ee2d8f2bbfa73a8c5d28f3f4ab1179ec3259d8a4
Gilsabo <gilsala_88@hotmail.com>
2023-10-27 16:47:05 +0200

    Add Form to cerate profile

---
+++ app/dashboard/profile/[username]/Profile.tsx
@@ -0,0 +1,41 @@
+import styles from './Profile.module.css';
+import UploadProfileImage from './UploadProfileImage';
+
+export default function Profile() {
+  return (
+    <div className={styles.mainContainer}>
+      <div>Profile</div>
+      <form className={styles.form}>
+        <label>
+          Player
+          <input />
+        </label>
+        <label>
+          Age
+          <input />
+        </label>
+        <label>
+          Years of experience
+          <input />
+        </label>
+        <label>
+          Dominant hand
+          <input />
+        </label>
+        <label>
+          E-mail
+          <input />
+        </label>
+        <label>
+          Password
+          <input />
+        </label>
+        <label>
+          Repeat passowrd
+          <input />
+        </label>
+        <UploadProfileImage />
+      </form>
+    </div>
+  );
+}
