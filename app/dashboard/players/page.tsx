1fc02359252282ca7050b0a8440ac8f402ac2833
Gilsabo <gilsala_88@hotmail.com>
2023-10-27 15:34:04 +0200

    Change file to tsx and render proper properties from users

--- app/dashboard/players/page.tsx
+++ app/dashboard/players/page.tsx
@@ -3,23 +3,24 @@ import { getUsers } from '../../../database/users';
 export default async function Players() {
   const users = await getUsers();
   console.log(users);
-
   return (
     <div>
       <div>Players</div>
       <div>
         {users.map((user) => {
           return (
-            <div key={`div-players-${user.lastName}`}>
-              <div>{user.userName}</div>
+            <div key={`div-players-${user.id}`}>
+              {/* <div>{user.userName}</div>
               <div> {user.age}</div>
-              <div> {user.dominantHand}</div>
-              <img
+              <div> {user.dominantHand}</div> */}
+              {/* <img
                 src={`../images/${user.profilePictureUrl}.jpeg`}
                 alt={`${user.firstName}${user.lastName}`}
                 width={100}
                 height={100}
-              />
+              /> */}
+              <div>{user.id}</div>
+              <div>{user.userName}</div>
             </div>
           );
         })}
