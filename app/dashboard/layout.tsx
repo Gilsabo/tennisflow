395c32093695ae1b2b841584fcbe0c1c02d137c4
Gilsabo <gilsala_88@hotmail.com>
2023-10-27 15:09:59 +0200

    Change file to tsx add types and change link when user not loggedin

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,93 @@
+import { cookies } from 'next/headers';
+import Link from 'next/link';
+import { ReactNode } from 'react';
+import LogoutButton from '../(auth)/logout/LogoutButton';
+import { getUserBySessionToken } from '../../database/users';
+import styles from './dashBoard.module.css';
+
+type Props = {
+  children: ReactNode;
+};
+
+export default async function Layout(props: Props) {
+  const cookieStore = cookies();
+  const sessionToken = cookieStore.get('sessionToken');
+
+  const user =
+    sessionToken && (await getUserBySessionToken(sessionToken.value));
+
+  //   console.log('Check Xpath: ', headersList.get('x-pathname'));
+
+  // // User has to be an admin
+  // // Get user from the database that meets the admin requirements
+
+  // // 3. Either redirect or render the login form
+  // if (!session) redirect(`/login?returnTo=${headersList.get('x-pathname')}`);
+
+  return (
+    <>
+      <div>Dashboard</div>
+      <header className={styles.header}>
+        <div className={styles.logoSection}>
+          <div>tennisoverflow - </div>
+          <span>- logo </span>
+        </div>
+
+        <nav>
+          <form action="/search" method="get">
+            <label>
+              Search:
+              <input name="search" placeholder="Enter your search..." />
+            </label>
+            <button type="button">Search</button>
+          </form>
+        </nav>
+        <label>
+          Upload:
+          <input type="file" />
+        </label>
+        <a href="/">notification</a>
+        {user ? <div>{user.userName}</div> : ''}
+        <LogoutButton />
+        <Link href="/dashboard/admin">Admin</Link>
+      </header>
+      <main className={styles.mainContainer}>
+        <div>
+          <ul>
+            <li>
+              <Link href="/dashboard"> Home</Link>
+            </li>
+            <li>
+              <Link href="/dashboard/history"> History</Link>
+            </li>
+            <li>
+              {user ? (
+                <Link
+                  href={{ pathname: `/dashboard/profile/${user.userName}` }}
+                >
+                  Profile
+                </Link>
+              ) : (
+                <Link href="/login"> Profile</Link>
+              )}
+            </li>
+            <li>
+              <Link href="/dashboard/my-videos"> My videos</Link>
+            </li>
+            <li>
+              <Link href="/dashboard/players"> Players</Link>
+            </li>
+
+            <li>
+              <Link href="/dashboard/most-commented"> Most commented</Link>
+            </li>
+            <li>
+              <Link href="/dashboard/most-viewed"> Most viewed</Link>
+            </li>
+          </ul>
+        </div>
+        {props.children}
+      </main>
+    </>
+  );
+}
