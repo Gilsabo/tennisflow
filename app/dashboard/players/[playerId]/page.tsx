ef5c6d5768cd564cfa391db9023e51eec57e8d59
Gilsabo <gilsala_88@hotmail.com>
2023-10-29 07:46:29 +0100

    Render single player

--- app/dashboard/players/[playerId]/page.tsx
+++ app/dashboard/players/[playerId]/page.tsx
@@ -1,3 +1,33 @@
-export default function PlayerPage() {
-  return <div>PlayerPage</div>;
+import { notFound } from 'next/navigation';
+import { getUserProfileById } from '../../../../database/profiles';
+
+type Props = {
+  params: {
+    playerId: string;
+  };
+};
+
+export default async function PlayerPage(props: Props) {
+  const singlePlayer = await getUserProfileById(Number(props.params.playerId));
+
+  if (!singlePlayer) {
+    return notFound();
+  }
+  return (
+    <>
+      <div>PlayerPage</div>
+      <div>{singlePlayer.firstName}</div>
+      <div>{singlePlayer.lastName}</div>
+      <div>{singlePlayer.age}</div>
+      <div>{singlePlayer.yearsExperience}</div>
+      <div>{singlePlayer.dominantHand}</div>
+      <div>{singlePlayer.description}</div>
+      <img
+        src={`./../../images/${singlePlayer.profilePictureUrl}.jpeg`}
+        alt={`${singlePlayer.firstName}${singlePlayer.lastName}`}
+        width={100}
+        height={100}
+      />
+    </>
+  );
 }
