49c41a1819f6fbc27e7197cbf27f1b6db9fb69a8
Gilsabo <gilsala_88@hotmail.com>
2023-10-29 07:45:32 +0100

    Add function to get profile by id

--- database/profiles.ts
+++ database/profiles.ts
@@ -9,3 +9,16 @@ export const getUserProfiles = cache(async () => {
   `;
   return userProfiles;
 });
+
+export const getUserProfileById = cache(async (id: number) => {
+  // Postgres always returns an array
+  const [userProfile] = await sql<UserProfile[]>`
+    SELECT
+      *
+    FROM
+      user_profiles
+    WHERE
+      id = ${id}
+  `;
+  return userProfile;
+});
