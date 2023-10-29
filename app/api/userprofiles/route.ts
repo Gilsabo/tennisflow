374dda50d8998f20a9209101f3be3435e35a5b0f
Gilsabo <gilsala_88@hotmail.com>
2023-10-29 17:40:01 +0100

    Add api userprofiles

---
+++ app/api/userprofiles/route.ts
@@ -0,0 +1,79 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { z } from 'zod';
+import { createUserProfile } from '../../../database/profiles';
+import { UserProfile } from '../../../migrations/00002-createTableUserProfiles';
+
+// id: number;
+// firstName: string;
+// lastName: string;
+// email: string;
+// age: number;
+// yearsExperience: number;
+// dominantHand: string;
+// description: string;
+// profilePictureUrl: string | null;
+type Error = {
+  error: string;
+};
+
+type UserProfilesResponseBodyGet =
+  | {
+      userProfile: UserProfile;
+    }
+  | Error;
+
+const userProfilsSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string(),
+  age: z.number(),
+  yearsExperience: z.number(),
+  dominantHand: z.string(),
+  description: z.string(),
+  profilePictureUrl: z.string().optional(),
+  userId: z.number(),
+});
+
+export async function POST(
+  request: NextRequest,
+): Promise<NextResponse<UserProfilesResponseBodyGet>> {
+  const body = await request.json();
+
+  const result = userProfilsSchema.safeParse(body);
+
+  if (!result.success) {
+    // zod send you details about the error
+    // console.log(result.error);
+    return NextResponse.json(
+      {
+        error: 'The data is incomplete',
+      },
+      { status: 400 },
+    );
+  }
+  // Get the animals from the database
+  const userProfile = await createUserProfile(
+    result.data.firstName,
+    result.data.lastName,
+    result.data.email,
+    result.data.age,
+    result.data.yearsExperience,
+    result.data.dominantHand,
+    result.data.description,
+    result.data.profilePictureUrl,
+    result.data.userId,
+  );
+
+  if (!userProfile) {
+    return NextResponse.json(
+      {
+        error: 'Error creating the new animal',
+      },
+      { status: 500 },
+    );
+  }
+
+  return NextResponse.json({
+    userProfile: userProfile,
+  });
+}
