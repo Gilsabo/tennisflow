35a61e26802041529b3e9c41d11aadb8a3319393
Gilsabo <gilsala_88@hotmail.com>
2023-10-29 10:33:24 +0100

    Comment out the file to see if works without it

--- app/dashboard/profile/[username]/UploadProfileImage.tsx
+++ app/dashboard/profile/[username]/UploadProfileImage.tsx
@@ -1,47 +1,47 @@
-'use client';
-import { useState } from 'react';
+// 'use client';
+// import { useState } from 'react';
 
-export default function UploadProfileImage() {
-  const [imageSelected, setImageSelected] = useState<File>();
-  const uploadImage = () => {
-    if (!imageSelected) {
-      console.error('No image selected');
-      return;
-    }
-    const formData = new FormData();
-    formData.append('file', imageSelected);
-    formData.append('upload_preset', 'gm0xdnab');
-    fetch('https://api.cloudinary.com/v1_1/dqiq3eutn/image/upload', {
-      method: 'POST',
-      body: formData,
-    })
-      .then((response) => {
-        console.log(response);
-        return response.json(); // Assuming the response is in JSON format
-      })
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  };
+// export default function UploadProfileImage(props) {
+//   const [imageSelected, setImageSelected] = useState<File>();
+//   const uploadImage = () => {
+//     if (!imageSelected) {
+//       console.error('No image selected');
+//       return;
+//     }
+//     const formData = new FormData();
+//     formData.append('file', imageSelected);
+//     formData.append('upload_preset', 'gm0xdnab');
+//     fetch('https://api.cloudinary.com/v1_1/dqiq3eutn/image/upload', {
+//       method: 'POST',
+//       body: formData,
+//     })
+//       .then((response) => {
+//         console.log(response);
+//         return response.json(); // Assuming the response is in JSON format
+//       })
+//       .then((data) => {
+//         console.log(data);
+//       })
+//       .catch((error) => {
+//         console.error('Error:', error);
+//       });
+//   };
 
-  return (
-    <>
-      <label>
-        Insert profile image
-        <input
-          type="file"
-          onChange={(e) => {
-            if (!e.target.files) return;
-            setImageSelected(e.target.files[0]);
-          }}
-        />
-      </label>
-      <button type="button" onClick={uploadImage}>
-        Upload image
-      </button>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <label>
+//         Insert profile image
+//         <input
+//           type="file"
+//           onChange={(event) => {
+//             if (!event.target.files) return;
+//             setImageSelected(event.target.files[0]);
+//           }}
+//         />
+//       </label>
+//       <button type="button" onClick={uploadImage}>
+//         Upload image
+//       </button>
+//     </>
+//   );
+// }
