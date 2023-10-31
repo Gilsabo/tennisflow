ed05bbf1e488c62fde937fee82ff97b2fd8079ea
Gilsabo <gilsala_88@hotmail.com>
2023-10-31 19:11:24 +0100

    Add rendering depending if has profile

--- app/dashboard/profile/[username]/page.tsx
+++ app/dashboard/profile/[username]/page.tsx
@@ -1,4 +1,5 @@
 import { notFound } from 'next/navigation';
+import { getUserProfileByUserId } from '../../../../database/profiles';
 import { getUserIdByUserName } from '../../../../database/users';
 import Profile from './Profile';
 
@@ -15,6 +16,7 @@ export default async function UserProfilePage({ params }: Props) {
   // takes the id by passing the name. this is needed to create the profile. We need the id from users table and pass it as foreing
   // in user_profiles table so that we have the foreign kew
   const userId = await getUserIdByUserName(userNameWithoutURIelements);
+
   console.log('user', userId);
 
   // handel error when the profile does not match------------------>>>>>>>>
@@ -22,10 +24,27 @@ export default async function UserProfilePage({ params }: Props) {
     return notFound();
   }
 
+  const profilePlayer = await getUserProfileByUserId(userId.id);
+  console.log('profilePlayerrrr', profilePlayer);
+
   return (
     <div>
-      <h2>{userNameWithoutURIelements} Profile</h2>
-      <Profile userId={userId.id} />
+      {profilePlayer ? (
+        <div key={`div-profile-${profilePlayer.id}`}>
+          <div>{profilePlayer.firstName}</div>
+          <div>{profilePlayer.lastName}</div>
+          <div>{profilePlayer.email}</div>
+          <div>{profilePlayer.age}</div>
+          <div>{profilePlayer.yearsExperience}</div>
+          <div>{profilePlayer.dominantHand}</div>
+          <div>{profilePlayer.description}</div>
+        </div>
+      ) : (
+        <>
+          <h2>{userNameWithoutURIelements} Profile</h2>
+          <Profile userId={userId.id} />
+        </>
+      )}
     </div>
   );
 }
