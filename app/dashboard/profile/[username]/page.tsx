e42e69ce3c335de7da2af2f978feb0f560f00f13
Gilsabo <gilsala_88@hotmail.com>
2023-10-31 15:32:26 +0100

    Add page not found function

--- app/dashboard/profile/[username]/page.tsx
+++ app/dashboard/profile/[username]/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from 'next/navigation';
 import { getUserIdByUserName } from '../../../../database/users';
 import Profile from './Profile';
 
@@ -7,17 +8,18 @@ type Props = {
 
 export default async function UserProfilePage({ params }: Props) {
   // removes URICODE in case user typed user name with spaces OR OTHER CHARACTERS
+
+  console.log('params', params.username);
   const removeSpacesFromUri = decodeURIComponent(
     params.username.replace(/\+/g, ' '),
   );
-
-  const userId = await getUserIdByUserName(params.username);
-
+  const userId = await getUserIdByUserName(removeSpacesFromUri);
   console.log('user', userId);
 
   if (userId === undefined) {
-    return;
+    return notFound();
   }
+
   // handel error------------------>>>>>>>>
 
   return (
