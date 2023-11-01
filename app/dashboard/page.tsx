b4279913d9a0c70900e965bd2aa64dd48dfc59cd
Gilsabo <gilsala_88@hotmail.com>
2023-11-01 12:12:51 +0100

    Remove video component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,10 @@
 import styles from './dashBoard.module.css';
-import Video from './video';
 
 export default function Dashboard() {
   return (
     <div className={styles.videoContainer}>
       <div className={styles.video}>
-        <Video />
+        <div>Video</div>
       </div>
       <div className={styles.video}>
         <div>video</div>
