b54d0826d4392c9c87c8dcaf2a13f9f7eb2a60ae
Gilsabo <gilsala_88@hotmail.com>
2023-11-01 07:56:31 +0100

    Change input for textarea in description

--- app/dashboard/upload/VideoForm.tsx
+++ app/dashboard/upload/VideoForm.tsx
@@ -1,43 +1,50 @@
 'use client';
 import React, { useState } from 'react';
+import UploadVideo from './UploadVideo';
+import styles from './UploadVideoForm.module.css';
 
 export default function VideoForm() {
   const [titleInput, setTitleInput] = useState('');
   const [descriptionInput, setDescriptionInput] = useState('');
   const [tagsInput, setTagsInput] = useState('');
   const [locationInput, setLocationInput] = useState('');
+
   return (
-    <form>
-      <label>
-        Title
-        <input
-          value={titleInput}
-          onChange={(e) => setTitleInput(e.currentTarget.value)}
-          required
-        />
-      </label>
-      <label>
-        What do you want to improve?
-        <input
+    <div className={styles.mainContainer}>
+      <form className={styles.form}>
+        <label>
+          Title
+          <input
+            value={titleInput}
+            onChange={(e) => setTitleInput(e.currentTarget.value)}
+            required
+          />
+        </label>
+        <label>
+          Strokes to focus on
+          <input
+            value={tagsInput}
+            onChange={(e) => setTagsInput(e.currentTarget.value)}
+          />
+        </label>
+        <label>
+          Location
+          <input
+            value={locationInput}
+            onChange={(e) => setLocationInput(e.currentTarget.value)}
+          />
+        </label>
+        <textarea
+          rows={25}
+          cols={50}
+          maxLength={250}
           value={descriptionInput}
           onChange={(e) => setDescriptionInput(e.currentTarget.value)}
           required
         />
-      </label>
-      <label>
-        Strokes to focus on
-        <input
-          value={tagsInput}
-          onChange={(e) => setTagsInput(e.currentTarget.value)}
-        />
-      </label>
-      <label>
-        Strokes to focus on
-        <input
-          value={locationInput}
-          onChange={(e) => setLocationInput(e.currentTarget.value)}
-        />
-      </label>
-    </form>
+        <button>Upload</button>
+      </form>
+      <UploadVideo />
+    </div>
   );
 }
