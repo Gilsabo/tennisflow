5c583ab253b8c11d2fc072ef842cad29ebfda68b
Gilsabo <gilsala_88@hotmail.com>
2023-11-03 15:48:33 +0100

    Create component Editprofile with profilePlayer info

---
+++ app/dashboard/profile/[username]/EditProfile.tsx
@@ -0,0 +1,22 @@
+'use client';
+import { UserProfile } from '../../../../migrations/00002-createTableUserProfiles';
+
+type Props = {
+  profilePlayer: UserProfile;
+};
+
+export default function EditProfile(props: Props) {
+  return (
+    <div>
+      <div key={`div-profile-${props.profilePlayer.id}`}>
+        <div>Name: {props.profilePlayer.firstName}</div>
+        <div>Last Name: {props.profilePlayer.lastName}</div>
+        <div>Email: {props.profilePlayer.email}</div>
+        <div>Age: {props.profilePlayer.age}</div>
+        <div>Years of experience: {props.profilePlayer.yearsExperience}</div>
+        <div>Dominand hand: {props.profilePlayer.dominantHand}</div>
+        <div>About me: {props.profilePlayer.description}</div>
+      </div>
+    </div>
+  );
+}
