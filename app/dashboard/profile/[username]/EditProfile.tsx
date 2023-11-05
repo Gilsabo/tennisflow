0b21d6a045f19baa847538e07eed7f39e30e7d16
Gilsabo <gilsala_88@hotmail.com>
2023-11-05 08:20:05 +0100

    Create editable profile

--- app/dashboard/profile/[username]/EditProfile.tsx
+++ app/dashboard/profile/[username]/EditProfile.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useState } from 'react';
 import { UserProfile } from '../../../../migrations/00002-createTableUserProfiles';
 
 type Props = {
@@ -6,17 +7,173 @@ type Props = {
 };
 
 export default function EditProfile(props: Props) {
+  const [onEditId, setOnEditId] = useState(0);
+  const [onEditFirstNameInput, setOnEditFirstNameInput] = useState('');
+  const [onEditLastNameInput, setOnEditLastNameInput] = useState('');
+  const [onEditEmailInput, setOnEditEmailInput] = useState('');
+  const [onEditAgeInput, setOnEditAgeInput] = useState<number>();
+  const [onEditYearsExperienceInput, setOnEditYearsExperienceInput] =
+    useState<number>();
+  const [onEditDominantHandInput, setOnEditDominantHandInput] = useState('');
+  const [onEditDescriptionInput, setOnEditDescriptionInput] = useState('');
+  const [onEditProfilePictureUrlInput, setOnEditProfilePictureUrlInput] =
+    useState('');
+
+  async function updateProfileByUserId(id: number) {
+    const response = await fetch(`/api/userprofiles/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        firstName: onEditFirstNameInput,
+        lastName: onEditLastNameInput,
+        email: onEditEmailInput,
+        age: onEditAgeInput,
+        yearsExperience: onEditYearsExperienceInput,
+        dominantHand: onEditDominantHandInput,
+        description: onEditDescriptionInput,
+        profilePictureUrl: onEditProfilePictureUrlInput,
+      }),
+    });
+
+    const data = await response.json();
+
+    // setAnimalList(
+    //   animalList.map((animal) => {
+    //     if (animal.id === data.animal.id) {
+    // //       return data.animal;
+    // //     }
+    // //     return animal;
+    //   }),
+    // );
+  }
   return (
     <div>
-      <div key={`div-profile-${props.profilePlayer.id}`}>
-        <div>Name: {props.profilePlayer.firstName}</div>
-        <div>Last Name: {props.profilePlayer.lastName}</div>
-        <div>Email: {props.profilePlayer.email}</div>
-        <div>Age: {props.profilePlayer.age}</div>
-        <div>Years of experience: {props.profilePlayer.yearsExperience}</div>
-        <div>Dominant hand: {props.profilePlayer.dominantHand}</div>
-        <div>About me: {props.profilePlayer.description}</div>
-      </div>
+      <label>
+        Name:
+        <input
+          value={
+            props.profilePlayer.id !== onEditId
+              ? props.profilePlayer.firstName
+              : onEditFirstNameInput
+          }
+          onChange={(event) =>
+            setOnEditFirstNameInput(event.currentTarget.value)
+          }
+          disabled={props.profilePlayer.id !== onEditId}
+        />
+      </label>
+
+      <label>
+        Last name:
+        <input
+          value={
+            props.profilePlayer.id !== onEditId
+              ? props.profilePlayer.lastName
+              : onEditLastNameInput
+          }
+          onChange={(event) =>
+            setOnEditLastNameInput(event.currentTarget.value)
+          }
+          disabled={props.profilePlayer.id !== onEditId}
+        />
+      </label>
+
+      <label>
+        Email:
+        <input
+          value={
+            props.profilePlayer.id !== onEditId
+              ? props.profilePlayer.email
+              : onEditEmailInput
+          }
+          onChange={(event) => setOnEditEmailInput(event.currentTarget.value)}
+          disabled={props.profilePlayer.id !== onEditId}
+        />
+      </label>
+
+      <label>
+        Age:
+        <input
+          value={
+            props.profilePlayer.id !== onEditId
+              ? props.profilePlayer.age
+              : onEditAgeInput
+          }
+          onChange={(event) =>
+            setOnEditAgeInput(Number(event.currentTarget.value))
+          }
+          disabled={props.profilePlayer.id !== onEditId}
+        />
+      </label>
+
+      <label>
+        Years of experience:
+        <input
+          value={
+            props.profilePlayer.id !== onEditId
+              ? props.profilePlayer.yearsExperience
+              : onEditYearsExperienceInput
+          }
+          onChange={(event) =>
+            setOnEditYearsExperienceInput(Number(event.currentTarget.value))
+          }
+          disabled={props.profilePlayer.id !== onEditId}
+        />
+      </label>
+
+      <label>
+        Dominant hand:
+        <input
+          value={
+            props.profilePlayer.id !== onEditId
+              ? props.profilePlayer.dominantHand
+              : onEditDominantHandInput
+          }
+          onChange={(event) =>
+            setOnEditDominantHandInput(event.currentTarget.value)
+          }
+          disabled={props.profilePlayer.id !== onEditId}
+        />
+      </label>
+
+      <textarea
+        rows={25}
+        cols={50}
+        maxLength={250}
+        value={
+          props.profilePlayer.id !== onEditId
+            ? props.profilePlayer.description
+            : onEditDescriptionInput
+        }
+        onChange={(event) =>
+          setOnEditDescriptionInput(event.currentTarget.value)
+        }
+        disabled={props.profilePlayer.id !== onEditId}
+      />
+      {onEditId === props.profilePlayer.id ? (
+        <button
+          onClick={async () => {
+            // await updateProfileByUserId(props.profilePlayer.id);
+            setOnEditId(0);
+          }}
+        >
+          save
+        </button>
+      ) : (
+        <button
+          onClick={() => {
+            setOnEditFirstNameInput(props.profilePlayer.firstName);
+            setOnEditLastNameInput(props.profilePlayer.lastName);
+            setOnEditEmailInput(props.profilePlayer.email);
+            setOnEditAgeInput(Number(props.profilePlayer.age));
+            setOnEditAgeInput(Number(props.profilePlayer.yearsExperience));
+            setOnEditDominantHandInput(props.profilePlayer.dominantHand);
+            setOnEditDescriptionInput(props.profilePlayer.description);
+            setOnEditId(props.profilePlayer.id);
+          }}
+        >
+          Edit
+        </button>
+      )}
     </div>
   );
 }
