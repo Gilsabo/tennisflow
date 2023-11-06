8c92b26aa7ac45b8db966397a20863cb237cb788
Gilsabo <gilsala_88@hotmail.com>
2023-11-06 10:46:21 +0100

    Remove console.log

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,5 @@ import Vidoes from './Videos';
 
 export default async function Dashboard() {
   const videos = await getVideos();
-  console.log('videossss', videos);
   return <Vidoes videos={videos} />;
 }
