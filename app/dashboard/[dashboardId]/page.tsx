5d6ac2325b9c7389277a3061e3a852e5ef619ab0
Gilsabo <gilsala_88@hotmail.com>
2023-11-06 20:25:26 +0100

    Add comments and change redirect path

--- app/dashboard/[dashboardId]/page.tsx
+++ app/dashboard/[dashboardId]/page.tsx
@@ -1,6 +1,10 @@
-import { getUserProfileById } from '../../../database/profiles';
-// import { notFound } from 'next/navigation';
-// import { getUserProfileById } from '../../../database/profiles';
+import { cookies } from 'next/headers';
+import { notFound, redirect } from 'next/navigation';
+import {
+  getUserProfileById,
+  getUserProfileByUserId,
+} from '../../../database/profiles';
+import { getUserBySessionToken } from '../../../database/users';
 import { getVideoByIdVideo } from '../../../database/videos';
 import Comments from './Comments';
 
@@ -12,22 +16,39 @@ type Props = {
 
 export default async function DashboardVideoId(props: Props) {
   const imageURL = 'https://res.cloudinary.com/dqiq3eutn/video/upload/';
-  // const singleVideo = await getUserProfileById(
-  //   Number(props.params.dashboardId),
-  // );
+  console.log('params', props.params);
 
-  const singleVideos = await getVideoByIdVideo(
-    Number(props.params.dashboardId),
-  );
+  // get user name by session token in order to get the userProfileId of the person who is logged in and comments the video
+  const cookieStore = cookies();
+  const sessionToken = cookieStore.get('sessionToken');
 
+  const user =
+    sessionToken && (await getUserBySessionToken(sessionToken.value));
+
+  // if there is no one logged in or the user logs out, redirect it to login page
+  if (!user) redirect(`/login?returnTo=/dashboard/${props.params.dashboardId}`);
+
+  // Since we need the userProfild of the loggedin person, we need to take userProfile value of the logged in person
+  const userLoggedin = await getUserProfileByUserId(Number(user.id));
+  if (!userLoggedin) {
+    redirect(`/returnTo=/dashboard/${props.params.dashboardId}`);
+  }
+
+  // we take the table of the posted video to take the userProfileId and to render info of the video
+  const singleVideo = await getVideoByIdVideo(Number(props.params.dashboardId));
+  // we take the userProfileId to get info from the userProfile and render it on the page
   const userProfile = await getUserProfileById(
-    Number(singleVideos[0]?.userProfileId),
+    Number(singleVideo[0]?.userProfileId),
   );
-  console.log('userProfile', userProfile);
+
+  if (!userProfile) {
+    notFound();
+  }
+
   return (
     <div>
       <div>
-        <div>{singleVideos[0]?.title}</div>
+        <div>{singleVideo[0]?.title}</div>
 
         <video
           id="doc-player"
@@ -39,22 +60,16 @@ export default async function DashboardVideoId(props: Props) {
           height={500}
         >
           <source
-            src={`${imageURL}${singleVideos[0]?.videoUrl}`}
+            src={`${imageURL}${singleVideo[0]?.videoUrl}`}
             type="video/mp4"
           />
-          <track
-            kind="captions"
-            src="/captions.vtt"
-            srcLang="en"
-            label="English"
-            default
-          />
+          <track src="/captions.vtt" srcLang="en" label="English" default />
           Your browser does not support the video tag.
         </video>
-        <div>{userProfile?.firstName}</div>
-        <div>{userProfile?.lastName}</div>
+        <div>{userProfile.firstName}</div>
+        <div>{userProfile.lastName}</div>
       </div>
-      <Comments />
+      <Comments videoId={singleVideo[0]?.id} userProfileId={userLoggedin.id} />
     </div>
   );
 }
