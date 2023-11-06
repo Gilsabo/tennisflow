fbaba0a6b0601b112cb7a6efed59138aa7dce164
Gilsabo <gilsala_88@hotmail.com>
2023-11-06 20:28:04 +0100

    Change name commentUser

--- app/dashboard/[dashboardId]/Comments.tsx
+++ app/dashboard/[dashboardId]/Comments.tsx
@@ -2,13 +2,34 @@
 
 import { useState } from 'react';
 
-export default function Comments() {
+type Props = {
+  videoId: number | undefined;
+  userProfileId: number;
+};
+
+export default function Comments(props: Props) {
   const [commentInput, setCommentInput] = useState('');
+
+  async function createComment() {
+    const response = await fetch(`/api/comments`, {
+      method: 'POST',
+      body: JSON.stringify({
+        videoId: props.videoId,
+        userProfileId: props.userProfileId,
+        commentUser: commentInput,
+      }),
+    });
+    const data = await response.json();
+    console.log('commentsresponse', data);
+  }
+
   return (
     <div>
       <form
-        onSubmit={(e) => {
+        onSubmit={async (e) => {
           e.preventDefault();
+          await createComment();
+          setCommentInput('');
         }}
       >
         <label>
