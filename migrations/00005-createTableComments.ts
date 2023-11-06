8a6c29bfc4d84fae7ff0880baa86567aed219255
Gilsabo <gilsala_88@hotmail.com>
2023-11-06 15:08:46 +0100

    Add functions comments and type

--- migrations/00005-createTableComments.ts
+++ migrations/00005-createTableComments.ts
@@ -1,9 +1,22 @@
 import { Sql } from 'postgres';
 
+export type Comments = {
+  id: number;
+  video_id: number;
+  userProfileId: number;
+  comment: string;
+};
+
 export async function up(sql: Sql) {
   await sql`
     CREATE TABLE
-      comments ()
+      comments (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        video_id INTEGER NOT NULL REFERENCES videos,
+        user_profile_id INTEGER NOT NULL REFERENCES user_profiles (id) ON DELETE CASCADE,
+        comment VARCHAR(250) NOT NULL,
+        TIMESTAMP TIMESTAMPTZ NOT NULL
+      )
   `;
 }
 
