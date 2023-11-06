751820630d107a2daa958ad090fce6e43225c55c
Gilsabo <gilsala_88@hotmail.com>
2023-11-06 10:45:39 +0100

    Remove unnecessary div

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,8 @@
 import { getVideos } from '../../database/videos';
-import styles from './dashBoard.module.css';
 import Vidoes from './Videos';
 
 export default async function Dashboard() {
   const videos = await getVideos();
   console.log('videossss', videos);
-  return (
-    <div className={styles.videoContainer}>
-      <Vidoes videos={videos} />
-    </div>
-  );
+  return <Vidoes videos={videos} />;
 }
