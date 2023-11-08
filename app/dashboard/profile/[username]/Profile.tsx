a221bec80b2527c4453064e300995e0f90302d17
Gilsabo <gilsala_88@hotmail.com>
2023-11-08 08:15:09 +0100

    Fix type with years

--- app/dashboard/profile/[username]/Profile.tsx
+++ app/dashboard/profile/[username]/Profile.tsx
@@ -155,7 +155,7 @@ export default function Profile({ userId, userName }: Props) {
               htmlFor="years of experience"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
-              Yesars of experience
+              Years of experience
             </label>
             <div className="mt-2">
               <input
@@ -165,8 +165,8 @@ export default function Profile({ userId, userName }: Props) {
                 }
                 type="number"
                 required
-                name="Yesars of experience"
-                id="Yesars of experience"
+                name="Years of experience"
+                id="Years of experience"
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
