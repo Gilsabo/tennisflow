52072c4e26730b9de71150111f6430f509fce292
Gilsabo <gilsala_88@hotmail.com>
2023-11-08 08:39:21 +0100

    Add styles on the form with tailwind

--- app/dashboard/profile/[username]/EditProfile.tsx
+++ app/dashboard/profile/[username]/EditProfile.tsx
@@ -15,10 +15,17 @@ export default function EditProfile(props: Props) {
   const [onEditAgeInput, setOnEditAgeInput] = useState<number>();
   const [onEditYearsExperienceInput, setOnEditYearsExperienceInput] =
     useState<number>();
+  // const [dominantHandInput, setDominantHandInput] = useState('');
   const [onEditDominantHandInput, setOnEditDominantHandInput] = useState('');
   const [onEditDescriptionInput, setOnEditDescriptionInput] = useState('');
   const [onEditProfilePictureUrlInput, setOnEditProfilePictureUrlInput] =
-    useState<string | undefined>('hczauhey74t3zih407ll');
+    useState<string | undefined>('');
+
+  function handleDominandHand(event: React.ChangeEvent<HTMLInputElement>) {
+    // Updating the state with the selected radio button's value
+    setOnEditDominantHandInput(event.target.id);
+    console.log('dominant hand', onEditDominantHandInput);
+  }
 
   async function updateProfileByUserId(userName: string) {
     const response = await fetch(`/api/userprofiles/${userName}`, {
@@ -53,139 +60,268 @@ export default function EditProfile(props: Props) {
     // );
   }
   return (
-    <div>
-      <label>
-        Name:
-        <input
-          value={
-            props.profilePlayer.id !== onEditId
-              ? props.profilePlayer.firstName
-              : onEditFirstNameInput
-          }
-          onChange={(event) =>
-            setOnEditFirstNameInput(event.currentTarget.value)
-          }
-          disabled={props.profilePlayer.id !== onEditId}
-        />
-      </label>
+    <div className="ml-16 pl-20">
+      <div>
+        <div className="border-b mt-8 border-gray-900/10 pb-12 ">
+          <h2 className="text-base font-semibold leading-7 text-gray-900">
+            Edit your profile,
+            <span className="capitalize">{props.userName}</span>
+          </h2>
+          <p className="mt-1 text-sm leading-6 text-gray-600">
+            Use a permanent address where you can receive mail.
+          </p>
+        </div>
+        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="first-name"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              First name
+            </label>
+            <div className="mt-2">
+              <input
+                value={
+                  props.profilePlayer.id !== onEditId
+                    ? props.profilePlayer.firstName
+                    : onEditFirstNameInput
+                }
+                onChange={(event) =>
+                  setOnEditFirstNameInput(event.currentTarget.value)
+                }
+                disabled={props.profilePlayer.id !== onEditId}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="last-name"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Last name
+            </label>
+            <div className="mt-2">
+              <input
+                value={
+                  props.profilePlayer.id !== onEditId
+                    ? props.profilePlayer.lastName
+                    : onEditLastNameInput
+                }
+                onChange={(event) =>
+                  setOnEditLastNameInput(event.currentTarget.value)
+                }
+                disabled={props.profilePlayer.id !== onEditId}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                value={
+                  props.profilePlayer.id !== onEditId
+                    ? props.profilePlayer.email
+                    : onEditEmailInput
+                }
+                onChange={(event) =>
+                  setOnEditEmailInput(event.currentTarget.value)
+                }
+                disabled={props.profilePlayer.id !== onEditId}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-2 sm:col-start-1">
+            <label
+              htmlFor="age"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Age
+            </label>
+            <div className="mt-2">
+              <input
+                value={
+                  props.profilePlayer.id !== onEditId
+                    ? props.profilePlayer.age
+                    : onEditAgeInput
+                }
+                onChange={(event) =>
+                  setOnEditAgeInput(Number(event.currentTarget.value))
+                }
+                disabled={props.profilePlayer.id !== onEditId}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
 
-      <label>
-        Last name:
-        <input
-          value={
-            props.profilePlayer.id !== onEditId
-              ? props.profilePlayer.lastName
-              : onEditLastNameInput
-          }
-          onChange={(event) =>
-            setOnEditLastNameInput(event.currentTarget.value)
-          }
-          disabled={props.profilePlayer.id !== onEditId}
-        />
-      </label>
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="Years of experience"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Years of experience
+            </label>
+            <div className="mt-2">
+              <input
+                value={
+                  props.profilePlayer.id !== onEditId
+                    ? props.profilePlayer.yearsExperience
+                    : onEditYearsExperienceInput
+                }
+                onChange={(event) =>
+                  setOnEditYearsExperienceInput(
+                    Number(event.currentTarget.value),
+                  )
+                }
+                disabled={props.profilePlayer.id !== onEditId}
+                type="number"
+                required
+                name="Years of experience"
+                id="Years of experience"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
 
-      <label>
-        Email:
-        <input
-          value={
-            props.profilePlayer.id !== onEditId
-              ? props.profilePlayer.email
-              : onEditEmailInput
-          }
-          onChange={(event) => setOnEditEmailInput(event.currentTarget.value)}
-          disabled={props.profilePlayer.id !== onEditId}
-        />
-      </label>
+          <div className="border-b border-gray-900/10 pb-12 col-span-4">
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
+              Dominant hand
+            </h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              Chose your skillfull hand/s
+            </p>
 
-      <label>
-        Age:
-        <input
-          value={
-            props.profilePlayer.id !== onEditId
-              ? props.profilePlayer.age
-              : onEditAgeInput
-          }
-          onChange={(event) =>
-            setOnEditAgeInput(Number(event.currentTarget.value))
-          }
-          disabled={props.profilePlayer.id !== onEditId}
-        />
-      </label>
-
-      <label>
-        Years of experience:
-        <input
-          value={
-            props.profilePlayer.id !== onEditId
-              ? props.profilePlayer.yearsExperience
-              : onEditYearsExperienceInput
-          }
-          onChange={(event) =>
-            setOnEditYearsExperienceInput(Number(event.currentTarget.value))
-          }
-          disabled={props.profilePlayer.id !== onEditId}
-        />
-      </label>
-
-      <label>
-        Dominant hand:
-        <input
-          value={
-            props.profilePlayer.id !== onEditId
-              ? props.profilePlayer.dominantHand
-              : onEditDominantHandInput
-          }
-          onChange={(event) =>
-            setOnEditDominantHandInput(event.currentTarget.value)
-          }
-          disabled={props.profilePlayer.id !== onEditId}
-        />
-      </label>
-
-      <textarea
-        rows={25}
-        cols={50}
-        maxLength={250}
-        value={
-          props.profilePlayer.id !== onEditId
-            ? props.profilePlayer.description
-            : onEditDescriptionInput
-        }
-        onChange={(event) =>
-          setOnEditDescriptionInput(event.currentTarget.value)
-        }
-        disabled={props.profilePlayer.id !== onEditId}
-      />
-      {onEditId === props.profilePlayer.id ? (
-        <button
-          onClick={async () => {
-            await updateProfileByUserId(props.userName);
-            setOnEditId(0);
-          }}
-        >
-          save
-        </button>
-      ) : (
-        <button
-          onClick={() => {
-            setOnEditFirstNameInput(props.profilePlayer.firstName);
-            setOnEditLastNameInput(props.profilePlayer.lastName);
-            setOnEditEmailInput(props.profilePlayer.email);
-            setOnEditAgeInput(Number(props.profilePlayer.age));
-            setOnEditYearsExperienceInput(
-              Number(props.profilePlayer.yearsExperience),
-            );
-            setOnEditDominantHandInput(props.profilePlayer.dominantHand);
-            setOnEditDescriptionInput(props.profilePlayer.description);
-            setOnEditId(props.profilePlayer.id);
-            setOnEditProfilePictureUrlInput(
-              props.profilePlayer.profilePictureUrl,
-            );
-          }}
-        >
-          Edit
-        </button>
-      )}
+            <div className="mt-2 space-y-10 col-span-4">
+              <fieldset>
+                <div className="mt-2 space-y-6 flex items-start">
+                  <div className="flex items-center gap-x-3 mr-2 mt-6">
+                    <input
+                      id="right-handed"
+                      name="dominant-hand"
+                      type="radio"
+                      className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                      checked={onEditDominantHandInput === 'right-handed'}
+                      onChange={handleDominandHand}
+                      disabled={props.profilePlayer.id !== onEditId}
+                    />
+                    <label
+                      htmlFor="right-handed"
+                      className=" text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Right-handed
+                    </label>
+                  </div>
+                  <div className="flex items-center gap-x-3 mr-2 mt-0">
+                    <input
+                      id="left-handed"
+                      name="dominant-hand"
+                      type="radio"
+                      className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                      checked={onEditDominantHandInput === 'left-handed'}
+                      onChange={handleDominandHand}
+                      disabled={props.profilePlayer.id !== onEditId}
+                    />
+                    <label
+                      htmlFor="left-handed"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Left-handed
+                    </label>
+                  </div>
+                  <div className="flex items-center gap-x-3 mr-2 ">
+                    <input
+                      id="ambidextrous"
+                      name="dominant-hand"
+                      type="radio"
+                      className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                      checked={onEditDominantHandInput === 'ambidextrous'}
+                      onChange={handleDominandHand}
+                      disabled={props.profilePlayer.id !== onEditId}
+                    />
+                    <label
+                      htmlFor="ambidextrous"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Ambidextrous
+                    </label>
+                  </div>
+                </div>
+              </fieldset>
+            </div>
+            <div className="col-span-full mt-8">
+              <label
+                htmlFor="about"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                About
+              </label>
+              <div className="mt-2">
+                <textarea
+                  id="about"
+                  name="about"
+                  rows={3}
+                  maxLength={250}
+                  value={
+                    props.profilePlayer.id !== onEditId
+                      ? props.profilePlayer.description
+                      : onEditDescriptionInput
+                  }
+                  onChange={(event) =>
+                    setOnEditDescriptionInput(event.currentTarget.value)
+                  }
+                  disabled={props.profilePlayer.id !== onEditId}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+              <p className="mt-3 text-sm leading-6 text-gray-600">
+                Write a few sentences about your tennis journey.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="mt-6 flex items-center justify-start gap-x-6">
+          {onEditId === props.profilePlayer.id ? (
+            <button
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={async () => {
+                await updateProfileByUserId(props.userName);
+                setOnEditId(0);
+              }}
+            >
+              save
+            </button>
+          ) : (
+            <button
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={() => {
+                setOnEditFirstNameInput(props.profilePlayer.firstName);
+                setOnEditLastNameInput(props.profilePlayer.lastName);
+                setOnEditEmailInput(props.profilePlayer.email);
+                setOnEditAgeInput(Number(props.profilePlayer.age));
+                setOnEditYearsExperienceInput(
+                  Number(props.profilePlayer.yearsExperience),
+                );
+                setOnEditDominantHandInput(props.profilePlayer.dominantHand);
+                setOnEditDescriptionInput(props.profilePlayer.description);
+                setOnEditId(props.profilePlayer.id);
+                setOnEditProfilePictureUrlInput(
+                  props.profilePlayer.profilePictureUrl,
+                );
+              }}
+            >
+              Edit
+            </button>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
