e251a46d97d9443c38b38ce18be344880463de70
Gilsabo <gilsala_88@hotmail.com>
2023-11-08 15:33:03 +0100

    Add tailwind form

--- app/dashboard/upload/VideoForm.tsx
+++ app/dashboard/upload/VideoForm.tsx
@@ -1,7 +1,6 @@
 'use client';
 import React, { useState } from 'react';
 import UploadVideo from './UploadVideo';
-import styles from './UploadVideoForm.module.css';
 
 type Props = {
   userProfileId: number;
@@ -14,6 +13,20 @@ export default function VideoForm({ userProfileId }: Props) {
   const [locationInput, setLocationInput] = useState('');
   const [videoUrlInput, setVideoUrlInput] = useState('');
 
+  console.log('taaags', tagsInput);
+
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const checkboxId = e.target.id;
+
+    // If the checkbox is checked, add its id to the tagsInput state
+    if (e.target.checked) {
+      setTagsInput((prevTags) => [...prevTags, checkboxId]);
+    } else {
+      // If the checkbox is unchecked, remove its id from the tagsInput state
+      setTagsInput((prevTags) => prevTags.filter((tag) => tag !== checkboxId));
+    }
+  };
+
   const request = {
     videoUrl: videoUrlInput,
     title: titleInput,
@@ -23,6 +36,12 @@ export default function VideoForm({ userProfileId }: Props) {
     userProfileId: userProfileId,
   };
   console.log(request);
+
+  // function handleMultipleStrokes(e:) {
+  //   const options = [...e.target.selectedOptions];
+  //   const values = options.map((option) => option.value);
+  //   setTagsInput(values);
+  // }
 
   async function createVideo() {
     const response = await fetch('/api/uservideos', {
@@ -41,67 +60,248 @@ export default function VideoForm({ userProfileId }: Props) {
   }
 
   return (
-    <div className={styles.mainContainer}>
-      <form
-        className={styles.form}
-        onSubmit={async (event) => {
-          event.preventDefault();
-          await createVideo();
-        }}
-      >
-        <label>
-          Title
-          <input
-            value={titleInput}
-            onChange={(e) => setTitleInput(e.currentTarget.value)}
-            required
-          />
-        </label>
-        {/* <label>
-          Strokes to focus on
-          <input
-            value={tagsInput}
-            onChange={(e) => setTagsInput(e.currentTarget.value)}
-          />
-        </label> */}
-        <label>
-          Pick all the strokes you want to improve:
-          <select
-            multiple={true}
-            value={tagsInput}
-            onChange={(e) => {
-              const options = [...e.target.selectedOptions];
-              const values = options.map((option) => option.value);
-              setTagsInput(values);
-            }}
-          >
-            <option value="forhand">Forhand</option>
-            <option value="backhand">Backhand</option>
-            <option value="volley">Volley</option>
-            <option value="smash">Smash</option>
-            <option value="serve">Serve</option>
-            <option value="foot-work">foot-work</option>
-          </select>
-        </label>
+    <div className="border-b border-gray-900/10 pb-12 ml-16 pl-20 ">
+      <div className="pt-8">
+        <h2 className="text-base font-semibold leading-7 text-gray-900">
+          Upload your video
+        </h2>
+        <p className="mt-1 text-sm leading-6 text-gray-600">
+          Your are a little bit closer to get better. First upload the video and
+          then complete all the fields.
+        </p>
+        <UploadVideo setVideoUrlInput={setVideoUrlInput} />
+        <form
+          className="flex flex-col"
+          onSubmit={async (event) => {
+            event.preventDefault();
+            await createVideo();
+          }}
+        >
+          {/* <label className="block text-sm font-medium leading-6 text-gray-900">
+          Title */}
 
-        <label>
-          Location
-          <input
-            value={locationInput}
-            onChange={(e) => setLocationInput(e.currentTarget.value)}
-          />
-        </label>
-        <textarea
-          rows={25}
-          cols={50}
-          maxLength={250}
-          value={descriptionInput}
-          onChange={(e) => setDescriptionInput(e.currentTarget.value)}
-          required
-        />
-        <button>Submit</button>
-      </form>
-      <UploadVideo setVideoUrlInput={setVideoUrlInput} />
+          <div className="sm:col-span-3">
+            <label
+              htmlFor="title"
+              className="block text-sm font-medium mt-10 leading-6 text-gray-900"
+            >
+              Title of the video
+            </label>
+            <div className="mt-2">
+              <input
+                className="block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={titleInput}
+                onChange={(e) => setTitleInput(e.currentTarget.value)}
+                required
+              />
+            </div>
+          </div>
+          <div className="mt-10 space-y-10">
+            <fieldset>
+              <legend className="text-sm font-semibold leading-6 text-gray-900">
+                Pick all the strokes you want to improve
+              </legend>
+              <div className="mt-6 space-y-6 flex flex-wrap">
+                <div className="relative flex gap-x-3 mt-6 mr-4">
+                  <div className="flex h-6 items-center">
+                    <input
+                      id="forhand"
+                      name="forhand"
+                      type="checkbox"
+                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                      onChange={handleCheckboxChange}
+                    />
+                  </div>
+                  <div className="text-sm leading-6 ">
+                    <label
+                      htmlFor="forhand"
+                      className="font-medium text-gray-900 "
+                    >
+                      Forhand
+                    </label>
+                  </div>
+                </div>
+                <div className="relative flex gap-x-3 mr-4">
+                  <div className="flex h-6 items-center">
+                    <input
+                      id="backhand"
+                      name="backhand"
+                      type="checkbox"
+                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                      onChange={handleCheckboxChange}
+                    />
+                  </div>
+                  <div className="text-sm leading-6">
+                    <label
+                      htmlFor="backhand"
+                      className="font-medium text-gray-900"
+                    >
+                      Backhand
+                    </label>
+                  </div>
+                </div>
+                <div className="relative flex gap-x-3 mr-4">
+                  <div className="flex h-6 items-center">
+                    <input
+                      id="volley"
+                      name="volley"
+                      type="checkbox"
+                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                      onChange={handleCheckboxChange}
+                    />
+                  </div>
+                  <div className="text-sm leading-6">
+                    <label
+                      htmlFor="volley"
+                      className="font-medium text-gray-900"
+                    >
+                      Volley
+                    </label>
+                  </div>
+                </div>
+                <div className="relative flex gap-x-3 mr-4">
+                  <div className="flex h-6 items-center">
+                    <input
+                      id="smash"
+                      name="smash"
+                      type="checkbox"
+                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                      onChange={handleCheckboxChange}
+                    />
+                  </div>
+                  <div className="text-sm leading-6">
+                    <label
+                      htmlFor="smash"
+                      className="font-medium text-gray-900"
+                    >
+                      Smash
+                    </label>
+                  </div>
+                </div>
+                <div className="relative flex gap-x-3 mr-4">
+                  <div className="flex h-6 items-center">
+                    <input
+                      id="serve"
+                      name="serve"
+                      type="checkbox"
+                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                      onChange={handleCheckboxChange}
+                    />
+                  </div>
+                  <div className="text-sm leading-6">
+                    <label
+                      htmlFor="serve"
+                      className="font-medium text-gray-900"
+                    >
+                      Serve
+                    </label>
+                  </div>
+                </div>
+                <div className="relative flex gap-x-3 mr-4">
+                  <div className="flex h-6 items-center">
+                    <input
+                      id="foot-work"
+                      name="foot-work"
+                      type="checkbox"
+                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                      onChange={handleCheckboxChange}
+                    />
+                  </div>
+                  <div className="text-sm leading-6">
+                    <label
+                      htmlFor="foot-work"
+                      className="font-medium text-gray-900"
+                    >
+                      Foot-work
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </fieldset>
+          </div>
+          <div className="sm:col-span-3">
+            <label
+              htmlFor="title"
+              className="block text-sm font-medium mt-10 leading-6 text-gray-900"
+            >
+              Location
+            </label>
+            <div className="mt-2">
+              <input
+                className="block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={locationInput}
+                onChange={(e) => setLocationInput(e.currentTarget.value)}
+                required
+              />
+            </div>
+          </div>
+
+          <div className="col-span-full mt-8">
+            <label
+              htmlFor="about"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Describe what you want to improve
+            </label>
+            <div className="mt-2">
+              <textarea
+                id="about"
+                name="about"
+                rows={3}
+                maxLength={250}
+                value={descriptionInput}
+                onChange={(e) => setDescriptionInput(e.currentTarget.value)}
+                required
+                className="block w-3/4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <p className="mt-3 text-sm leading-6 text-gray-600">
+              Explain not only what you think it is, but also what sensations
+              when playing in the video.
+            </p>
+          </div>
+          {/* <label>
+            Pick all the strokes you want to improve:
+            <select
+              multiple={true}
+              value={tagsInput}
+              onChange={(e) => {
+                const options = [...e.target.selectedOptions];
+                const values = options.map((option) => option.value);
+                setTagsInput(values);
+              }}
+            >
+              <option value="forhand">Forhand</option>
+              <option value="backhand">Backhand</option>
+              <option value="volley">Volley</option>
+              <option value="smash">Smash</option>
+              <option value="serve">Serve</option>
+              <option value="foot-work">foot-work</option>
+            </select>
+          </label>
+
+          <label>
+            Location
+            <input
+              value={locationInput}
+              onChange={(e) => setLocationInput(e.currentTarget.value)}
+            />
+          </label>
+          <textarea
+            rows={25}
+            cols={50}
+            maxLength={250}
+            value={descriptionInput}
+            onChange={(e) => setDescriptionInput(e.currentTarget.value)}
+            required
+          /> */}
+          <div className="mt-6 flex items-center justify-start gap-x-6">
+            <button className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
