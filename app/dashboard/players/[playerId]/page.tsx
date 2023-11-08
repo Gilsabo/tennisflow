44ba927a7731b183fcf7f0973e82981ef0b0aab2
Gilsabo <gilsala_88@hotmail.com>
2023-11-08 17:55:28 +0100

    Add styles

--- app/dashboard/players/[playerId]/page.tsx
+++ app/dashboard/players/[playerId]/page.tsx
@@ -15,20 +15,29 @@ export default async function PlayerPage(props: Props) {
     return notFound();
   }
   return (
-    <div>
-      <h1>Player</h1>
-      <div>Name: {singlePlayer.firstName}</div>
-      <div>Last Name: {singlePlayer.lastName}</div>
-      <div>Age: {singlePlayer.age}</div>
-      <div>Yesars of experience: {singlePlayer.yearsExperience}</div>
-      <div>Dominant hand: {singlePlayer.dominantHand}</div>
-      <div>About me: {singlePlayer.description}</div>
-      <img
-        src={`${imageURL}${singlePlayer.profilePictureUrl}`}
-        alt={`${singlePlayer.firstName}${singlePlayer.lastName}`}
-        width={100}
-        height={100}
-      />
+    <div className="mt-20">
+      <div className="flex w-2/4 shadow-2xl  m-auto border rounded-3xl">
+        <img
+          src={`${imageURL}${singlePlayer.profilePictureUrl}`}
+          alt={`${singlePlayer.firstName}${singlePlayer.lastName}`}
+          width={200}
+          height={100}
+        />
+        <div className="p-4">
+          <div className="text-xl mb-8">"{singlePlayer.description}"</div>
+          <div className="flex flex-col">
+            <div className="text-sm">Name: {singlePlayer.firstName}</div>
+            <div className="text-sm">Last Name: {singlePlayer.lastName}</div>
+            <div className="text-sm">Age: {singlePlayer.age}</div>
+            <div className="text-sm">
+              Years of experience: {singlePlayer.yearsExperience}
+            </div>
+            <div className="text-sm">
+              Dominant hand: {singlePlayer.dominantHand}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
