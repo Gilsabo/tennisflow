93af0130b8307bac1cf841677dccc5d434e5820c
Gilsabo <gilsala_88@hotmail.com>
2023-11-09 18:50:50 +0100

    Style login form

--- app/(auth)/login/LoginForm.tsx
+++ app/(auth)/login/LoginForm.tsx
@@ -41,9 +41,59 @@ export default function LoginFrom(props: Props) {
   }
 
   return (
-    <>
-      <div>Login</div>
-      <form onSubmit={async (event) => await handleRegister(event)}>
+    <div className="m-auto bg-slate-200 pt-40 h-screen">
+      <form
+        className="border  bg-teal-800 rounded-lg  w-96 h-96 m-auto"
+        onSubmit={async (event) => await handleRegister(event)}
+      >
+        <div className="m-auto">
+          <div className="w-80 mt-10 mb-4 sm:col-span-3">
+            <label
+              htmlFor="first-name"
+              className=" ml-8 block text-sm font-medium leading-6 text-slate-200"
+            >
+              User name
+            </label>
+            <div className="mt-2">
+              <input
+                onChange={(event) => setUserName(event.currentTarget.value)}
+                name="user-name"
+                id="user-name"
+                autoComplete="given-name"
+                className=" ml-8 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+        </div>
+        <div className=" w-80 sm:col-span-3">
+          <label
+            htmlFor="first-name"
+            className="block text-sm ml-8 font-medium leading-6 text-slate-200"
+          >
+            Password
+          </label>
+          <div className="mt-2">
+            <input
+              type="password"
+              name="passowrd"
+              id="passowrd"
+              onChange={(event) => setPassword(event.currentTarget.value)}
+              autoComplete="given-name"
+              className="block w-full ml-8 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+
+          <button className="mt-8 w-60 m-16 rounded-md bg-slate-200 px-3 py-2 text-sm font-semibold text-teal-800 shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            Log in
+          </button>
+        </div>
+        {errors.map((error) => (
+          <div className="error" key={`error-${error.message}`}>
+            Error: {error.message}
+          </div>
+        ))}
+      </form>
+      {/* <form onSubmit={async (event) => await handleRegister(event)}>
         <label>
           User name
           <input onChange={(event) => setUserName(event.currentTarget.value)} />
@@ -61,7 +111,7 @@ export default function LoginFrom(props: Props) {
             Error: {error.message}
           </div>
         ))}
-      </form>
-    </>
+      </form> */}
+    </div>
   );
 }
