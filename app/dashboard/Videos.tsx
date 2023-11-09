601f2fc9063cab6352e942685c3a419a052acdc1
Gilsabo <gilsala_88@hotmail.com>
2023-11-09 11:07:00 +0100

    Remove comments and style text and size video

--- app/dashboard/Videos.tsx
+++ app/dashboard/Videos.tsx
@@ -13,35 +13,21 @@ const Videos = ({ videos }: Props) => {
   return (
     <div className="flex flex-wrap mt-8 justify-center">
       {videos.map((video) => (
-        <div key={`div-video${video.videoUrl}`}>
-          <video
-            className="rounded-lg"
-            id="doc-player"
-            controls
-            muted
-            // autoPlay
-
-            width={200}
-            height={400}
-          >
+        <div className="mr-2 ml-2 w-60 mb-4" key={`div-video${video.videoUrl}`}>
+          <video className="rounded-lg mb-1" id="doc-player" controls muted>
             <source
               src={`https://res.cloudinary.com/dqiq3eutn/video/upload/${video.videoUrl}`}
               type="video/mp4"
             />
             <track srcLang="en" label="English" default />
-            Your browser does not support the video tag.
           </video>
           <Link href={`/dashboard/${video.id}`}>
-            <div>Posted on: {video.timestamp.toDateString()}</div>
+            <div className="text-xs">
+              Posted on: {video.timestamp.toDateString()}
+            </div>
           </Link>
         </div>
       ))}
-      {/* <CldVideoPlayer
-        id="sea-turtle"
-        width="500"
-        height="500"
-        src="https://res.cloudinary.com/dqiq3eutn/video/upload/m6svjspu8t2wz68selw5.mp4"
-      /> */}
     </div>
   );
 };
