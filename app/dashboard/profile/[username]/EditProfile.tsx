0389d795bd3e09b574d4235820d6a13332158b15
Gilsabo <gilsala_88@hotmail.com>
2023-11-10 11:02:34 +0100

    Change color button

--- app/dashboard/profile/[username]/EditProfile.tsx
+++ app/dashboard/profile/[username]/EditProfile.tsx
@@ -300,7 +300,7 @@ export default function EditProfile(props: Props) {
             </button>
           ) : (
             <button
-              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="rounded-md bg-violet-900 mb-8 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               onClick={() => {
                 setOnEditFirstNameInput(props.profilePlayer.firstName);
                 setOnEditLastNameInput(props.profilePlayer.lastName);
