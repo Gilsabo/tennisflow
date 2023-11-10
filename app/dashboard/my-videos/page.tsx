a2551a41c6cf8f6c9d42dbf25401bac359177f21
Gilsabo <gilsala_88@hotmail.com>
2023-11-10 10:52:19 +0100

    Add styles to link

--- app/dashboard/my-videos/page.tsx
+++ app/dashboard/my-videos/page.tsx
@@ -1,5 +1,16 @@
+import Link from 'next/link';
 import React from 'react';
 
 export default function Myvidos() {
-  return <div>My vidos</div>;
+  return (
+    <div className="pt-20 flex flex-col items-center mb-10">
+      <div className="mb-8">My videos</div>
+      <Link
+        href="/dashboard/upload"
+        className=" bg-slate-200 rounded-lg  text-teal-800 border border-solid py-2 px-4"
+      >
+        Upload your video
+      </Link>
+    </div>
+  );
 }
