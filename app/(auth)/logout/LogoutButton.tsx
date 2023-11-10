0953927f39aad958cbfedcdd78e388f704fadd0c
Gilsabo <gilsala_88@hotmail.com>
2023-11-10 11:51:29 +0100

    Add style to link

--- app/(auth)/logout/LogoutButton.tsx
+++ app/(auth)/logout/LogoutButton.tsx
@@ -4,7 +4,10 @@ import { logout } from './actions';
 export default function LogoutButton() {
   return (
     <form>
-      <button className="logoutButton" formAction={logout}>
+      <button
+        className=" bg-slate-200 rounded-lg  text-teal-800 border border-solid py-2 px-4"
+        formAction={logout}
+      >
         Logout{' '}
       </button>
     </form>
