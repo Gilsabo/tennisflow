6d69f1bece9c531d53857c937d54c6244de081d4
Gilsabo <gilsala_88@hotmail.com>
2023-11-13 17:42:05 +0100

    Add type for VideoWithComments query

--- migrations/00004-createTableVideos.ts
+++ migrations/00004-createTableVideos.ts
@@ -11,23 +11,31 @@ export type Video = {
   userProfileId: number;
 };
 
+export type VideoWithComments = {
+  commentId: string;
+  commentUser: string;
+  commentTimestamp: string;
+  userProfileFirstName: string;
+  userProfileLastName: string;
+  userProfileProfilePictureUrl: string | null;
+};
+
 export async function up(sql: Sql) {
   await sql`
-  CREATE TABLE videos(
-    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    video_url varchar(250) NOT NULL UNIQUE,
-    title varchar(100) NOT NULL,
-    description varchar(250) NOT NULL,
-    tags JSONB,
-    location varchar(30),
-    timestamp TIMESTAMPTZ NOT NULL,
-    user_profile_id integer NOT NULL REFERENCES user_profiles (id) ON DELETE CASCADE
-  )
+    CREATE TABLE
+      videos (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        video_url VARCHAR(250) NOT NULL UNIQUE,
+        title VARCHAR(100) NOT NULL,
+        description VARCHAR(250) NOT NULL,
+        tags JSONB,
+        location VARCHAR(30),
+        TIMESTAMP TIMESTAMPTZ NOT NULL,
+        user_profile_id INTEGER NOT NULL REFERENCES user_profiles (id) ON DELETE CASCADE
+      )
   `;
 }
 
 export async function down(sql: Sql) {
-  await sql`
-    DROP TABLE videos
-  `;
+  await sql` DROP TABLE videos `;
 }
