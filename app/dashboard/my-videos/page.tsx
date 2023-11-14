bd48bf4da37531ce35cf29ea88b94c6ce976d270
Gilsabo <gilsala_88@hotmail.com>
2023-11-14 15:39:24 +0100

    Fix typo

--- app/dashboard/my-videos/page.tsx
+++ app/dashboard/my-videos/page.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 
-export default function Myvidos() {
+export default function Myvideos() {
   return (
     <div className="pt-20 flex flex-col items-center mb-10">
       <div className="mb-8">My videos</div>
