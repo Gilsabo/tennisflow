20af57bb1cea2182d0a58141321d010bd276b44d
Gilsabo <gilsala_88@hotmail.com>
2023-11-15 18:38:27 +0100

    Style title

--- app/dashboard/most-commented/page.tsx
+++ app/dashboard/most-commented/page.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function MostCommented() {
-  return <div>MostCommented</div>;
+  return (
+    <div className="pt-20 flex flex-col items-center mb-10">
+      <div className="mb-8">Most commented</div>
+    </div>
+  );
 }
--- app/dashboard/most-viewed/page.tsx
+++ app/dashboard/most-viewed/page.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Mostviewed() {
-  return <div>Mostviewed</div>;
+  return (
+    <div className="pt-20 flex flex-col items-center mb-10">
+      <div className="mb-8">Most viewed</div>
+    </div>
+  );
 }
