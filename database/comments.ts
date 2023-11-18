06745fde547a2c72c336809adb93940324e80a92
Gilsabo <gilsala_88@hotmail.com>
2023-11-18 22:09:56 +0100

    Create join query

--- database/comments.ts
+++ database/comments.ts
@@ -1,5 +1,8 @@
 import { cache } from 'react';
-import { Comment } from '../migrations/00005-createTableComments';
+import {
+  Comment,
+  CommentsVideo,
+} from '../migrations/00005-createTableComments';
 import { sql } from './connect';
 
 export const createComent = cache(
@@ -23,3 +26,21 @@ export const createComent = cache(
     return comment;
   },
 );
+
+export const getAllCommentsFromEachVideo = cache(async () => {
+  const commentsFromVideo = await sql<CommentsVideo[]>`
+    SELECT
+      videos.title,
+      videos.id AS video_id,
+      videos.tags,
+      comments.comment_user AS comment_user,
+      user_profiles.first_name,
+      user_profiles.last_name,
+      user_profiles.profile_picture_url
+    FROM
+      comments
+      INNER JOIN videos ON videos.id = comments.video_id
+      INNER JOIN user_profiles ON user_profiles.id = videos.user_profile_id
+  `;
+  return commentsFromVideo;
+});
