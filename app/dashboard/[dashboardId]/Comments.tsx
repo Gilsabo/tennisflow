9c740c5034a49f1d48a656c115ca0f6ec8df57bf
Gilsabo <gilsala_88@hotmail.com>
2023-11-18 22:07:40 +0100

    Change color purple from textarea

--- app/dashboard/[dashboardId]/Comments.tsx
+++ app/dashboard/[dashboardId]/Comments.tsx
@@ -86,7 +86,7 @@ export default function Comments(props: Props) {
                 name="first-name"
                 id="first-name"
                 autoComplete="given-name"
-                className="block w-80 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-80 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-900 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
