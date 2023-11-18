84e244b1195c9d246b9d906201ef25f6f9431d2e
Gilsabo <gilsala_88@hotmail.com>
2023-11-18 09:04:53 +0100

    Inver colors when hovering

--- app/(auth)/logout/LogoutButton.tsx
+++ app/(auth)/logout/LogoutButton.tsx
@@ -5,7 +5,7 @@ export default function LogoutButton() {
   return (
     <form>
       <button
-        className=" bg-slate-200 rounded-lg  text-teal-800 border border-solid py-2 px-4"
+        className=" bg-slate-200 rounded-lg  text-[#00503C] border border-solid py-2 px-4 hover:bg-[#00503C] hover:text-slate-200"
         formAction={logout}
       >
         Logout{' '}
