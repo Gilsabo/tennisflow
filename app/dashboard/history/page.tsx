1e84bf1cb9b2cc15c1fefb4e96acb11a98375fec
Gilsabo <gilsala_88@hotmail.com>
2023-11-19 07:32:06 +0100

    Change title

--- app/dashboard/history/page.tsx
+++ app/dashboard/history/page.tsx
@@ -1,7 +1,7 @@
 export default function History() {
   return (
     <div className="pt-20 flex flex-col items-center mb-10">
-      <div className="mb-8">Most viewed</div>
+      <div className="mb-8">History</div>
     </div>
   );
 }
