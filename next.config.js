052cc5bbe52c9b2d362017d982a8568427e1c263
Gilsabo <gilsala_88@hotmail.com>
2023-11-21 16:35:16 +0100

    Add config for deployement and for cloudinary

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/dqiq3eutn/image/upload/**',
+      },
+    ],
+  },
   experimental: {
     typedRoutes: true,
   },
