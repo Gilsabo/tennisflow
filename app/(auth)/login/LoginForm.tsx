e0fd200f8f0651504c314ede48f0ce3e91711673
Gilsabo <gilsala_88@hotmail.com>
2023-12-27 16:49:52 +0100

    Turn form rensposive

--- app/(auth)/login/LoginForm.tsx
+++ app/(auth)/login/LoginForm.tsx
@@ -40,7 +40,7 @@ export default function LoginFrom(props: Props) {
 
   return (
     <main className="m-auto bg-slate-200 pt-40 h-screen">
-      <div className="mx-8">
+      <div className="mx-8 sm:mx-16 md:mx-32 lg:mx-80 xl:mx-64 xl:px-60 ">
         <div className="flex text-lg font-semibold text-[#00503C] mb-4">
           <Image
             className="mr-3 w-8 h-8 ml-7 bg-[#00503C] text-[#00503C] rounded-full "
