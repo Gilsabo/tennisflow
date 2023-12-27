d2d9452412135c97200926b3df5f2c54f29091df
Gilsabo <gilsala_88@hotmail.com>
2023-12-27 17:27:06 +0100

    Turn form responsive

--- app/(auth)/register/RegistrationForm.tsx
+++ app/(auth)/register/RegistrationForm.tsx
@@ -33,69 +33,71 @@ export default function RegistrationForm() {
   }
 
   return (
-    <div className="m-auto bg-slate-200 pt-40 h-screen">
-      <div className=" flex text-lg font-semibold text-[#00503C]">
-        <Image
-          className="mr-3 w-8 h-8 ml-7 bg-[#00503C] text-[#00503C] rounded-full "
-          src={tennisFlowLogo}
-          alt="tennisflow logo"
-        />
-        <div>Create an account</div>
-      </div>
-      <form
-        className="w-96 h-96 m-auto"
-        onSubmit={async (event) => await handleRegister(event)}
-      >
-        <div className="m-auto">
-          <div className="w-80 mt-10 mb-4 sm:col-span-3">
+    <main className="m-auto bg-slate-200 pt-40 h-screen">
+      <div className="mx-8 sm:mx-16 md:mx-32 lg:mx-80 xl:mx-64 xl:px-60 min-w-min">
+        <div className=" flex text-lg font-semibold text-[#00503C] mb-4">
+          <Image
+            className="mr-3 w-8 h-8 ml-7 bg-[#00503C] text-[#00503C] rounded-full "
+            src={tennisFlowLogo}
+            alt="tennisflow logo"
+          />
+          <div>Create an account</div>
+        </div>
+        <form
+          className="mx-8"
+          onSubmit={async (event) => await handleRegister(event)}
+        >
+          <div className="">
+            <div className="">
+              <label
+                htmlFor="first-name"
+                className="block text-sm font-medium text-[#00503C]"
+              >
+                User name
+              </label>
+              <div className="mt-2">
+                <input
+                  onChange={(event) => setUserName(event.currentTarget.value)}
+                  name="user-name"
+                  id="user-name"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md border-0 py-1.5 text-[#00503C] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-300  sm:text-sm"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="">
             <label
               htmlFor="first-name"
-              className=" ml-8 block text-sm font-medium text-[#00503C]"
+              className="block mt-4 text-sm font-medium text-[#00503C]"
             >
-              User name
+              Password
             </label>
             <div className="mt-2">
               <input
-                onChange={(event) => setUserName(event.currentTarget.value)}
-                name="user-name"
-                id="user-name"
+                type="password"
+                name="passowrd"
+                id="passowrd"
+                onChange={(event) => setPassword(event.currentTarget.value)}
                 autoComplete="given-name"
-                className=" ml-8 block w-full rounded-md border-0 py-1.5 text-[#00503C] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-300 sm:text-sm"
+                className="block w-full rounded-md border-0 py-1.5 text-[#00503C] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 hover:text-slate-200 focus:ring-2 focus:ring-inset focus:ring-gray-300 sm:text-sm"
               />
             </div>
-          </div>
-        </div>
-        <div className=" w-80 sm:col-span-3">
-          <label
-            htmlFor="first-name"
-            className="block text-sm ml-8 font-medium text-[#00503C]"
-          >
-            Password
-          </label>
-          <div className="mt-2">
-            <input
-              type="password"
-              name="passowrd"
-              id="passowrd"
-              onChange={(event) => setPassword(event.currentTarget.value)}
-              autoComplete="given-name"
-              className="block w-full ml-8 rounded-md border-0 py-1.5 text-[#00503C] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 hover:text-slate-200 focus:ring-2 focus:ring-inset focus:ring-gray-300 sm:text-sm"
-            />
-          </div>
 
-          <button className="mt-8 w-60 m-16 rounded-md bg-slate-200 border border-solid border-[#00503C] px-3 py-2 text-sm font-semibold text-[#00503C] shadow-sm hover:bg-[#00503C] hover:text-slate-200  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-300  ">
-            Register
-          </button>
-        </div>
-        {errors.map((error) => (
-          <div
-            className="p-4 rounded-xl w-80 m-auto text-center border-rose-600 text-rose-600 bg-slate-200"
-            key={`error-${error.message}`}
-          >
-            Error: {error.message}
+            <button className="mt-8 w-full rounded-md hover:bg-[#00503C] border border-solid border-[#00503C] px-3 py-2 text-sm font-semibold text-[#00503C] shadow-sm bg-slate-200 hover:text-slate-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-300">
+              Register
+            </button>
           </div>
-        ))}
-      </form>
-    </div>
+          {errors.map((error) => (
+            <div
+              className="p-4 rounded-xl w-80 m-auto text-center border-rose-600 text-rose-600 bg-slate-200"
+              key={`error-${error.message}`}
+            >
+              Error: {error.message}
+            </div>
+          ))}
+        </form>
+      </div>
+    </main>
   );
 }
