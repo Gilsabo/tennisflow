82442477402df63ac7f10fc63edc12330dc01caf
Gilsabo <gilsala_88@hotmail.com>
2024-01-04 19:17:25 +0100

    Add e2e tests

--- playwright/navigation.spec.ts
+++ playwright/navigation.spec.ts
@@ -1,5 +1,39 @@
-import test from '@playwright/test';
+import test, { expect } from '@playwright/test';
 
-test('navigation test', async ({ page }) => {
+test('navigation login', async ({ page }) => {
   await page.goto('http://localhost:3000');
+
+  expect(page).toHaveTitle(/Tennisflow | all about tennis/);
+
+  await page.getByRole('link', { name: 'Log in' }).click();
+  await expect(page).toHaveURL(/.*login/);
+});
+
+test('navigation login to profile', async ({ page }) => {
+  await page.goto('http://localhost:3000/login');
+
+  const userNameInput = page.locator('#user-name');
+  await userNameInput.fill('your-username');
+
+  const userPasswordInput = page.locator('#passowrd');
+  await userPasswordInput.fill('your-username');
+  await page.getByRole('button', { name: 'Log in' }).click();
+});
+
+test('navigation register', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  expect(page).toHaveTitle(/Tennisflow | all about tennis/);
+
+  await page.getByRole('link', { name: 'Sign up' }).click();
+  await expect(page).toHaveURL(/.*register/);
+});
+
+test('profile form', async ({ page }) => {
+  await page.goto('http://localhost:3000/dashboard/profile/gil');
+  await page.getByTestId('profile-first-name').click();
+  await page.getByTestId('profile-first-name').fill('Gil');
+  await page.getByTestId('profile-first-name').press('Tab');
+  await page.getByTestId('profile-last-name').fill('Sala');
+  await page.getByTestId('profile-last-name').press('Tab');
 });
