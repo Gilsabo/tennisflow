ed7b9fe256809f414c6e5f061fb1657d513b1b0e
Gilsabo <gilsala_88@hotmail.com>
2024-01-30 16:00:49 +0100

    Add side client component delelte button

---
+++ app/dashboard/my-videos/DeleteVideoButton.tsx
@@ -0,0 +1,24 @@
+'use client';
+import { useRouter } from 'next/navigation';
+
+export default function DeleteVideoButton() {
+  const router = useRouter();
+  return (
+    <button
+      onClick={() => {
+        const confirmed = window.confirm(
+          'Are you sure you want to delete your profile? This action cannot be undone.',
+        );
+
+        if (confirmed) {
+          router.push('/');
+        } else {
+          console.log('Deletion canceled');
+        }
+      }}
+      className="rounded-md bg-slate-200 hover:border-red-500 mb-8 px-3 py-2 text-sm font-semibold text-red-500 shadow-sm  hover:text-slate-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 hover:bg-red-500"
+    >
+      delete
+    </button>
+  );
+}
