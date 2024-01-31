7f80eaa42d1b60a5d6fa7152dae8a6005159a754
Gilsabo <gilsala_88@hotmail.com>
2024-01-31 09:04:15 +0100

    Add delete end point

---
+++ app/api/deleteusers/[deleteuserUsername]/route.ts
@@ -0,0 +1,53 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { z } from 'zod';
+import { deleteUser } from '../../../../database/users';
+import { User } from '../../../../migrations/00000-createTableUsers';
+
+export type Error = {
+  error: string;
+};
+
+export type UserNameResponseBodyDelete =
+  | {
+      user: User;
+    }
+  | Error;
+
+const userSchema = z.object({
+  id: z.number(),
+  userName: z.string(),
+});
+
+export async function DELETE(
+  request: NextRequest,
+): Promise<NextResponse<UserNameResponseBodyDelete>> {
+  const body = await request.json();
+
+  const result = userSchema.safeParse(body);
+
+  if (!result.success) {
+    // zod send you details about the error
+
+    return NextResponse.json(
+      {
+        error: 'userName does not exist or is incorrect',
+      },
+      { status: 400 },
+    );
+  }
+  console.log('result,', result);
+  const deleteUserInformations = await deleteUser(result.data.userName);
+
+  if (!deleteUserInformations) {
+    return NextResponse.json(
+      {
+        error: 'Error deleting user',
+      },
+      { status: 500 },
+    );
+  }
+
+  return NextResponse.json({
+    user: deleteUserInformations,
+  });
+}
