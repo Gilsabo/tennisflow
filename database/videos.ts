4e2ce59cdfe2a118536f7c182b8b747bca428729
Gilsabo <gilsala_88@hotmail.com>
2024-02-07 16:26:24 +0100

    Change type tages to only string

--- database/videos.ts
+++ database/videos.ts
@@ -10,7 +10,7 @@ export const createUserVideo = cache(
     videoUrl: string,
     title: string,
     description: string,
-    tags: string[] | null,
+    tags: string[],
     location: string,
     userProfileId: number,
   ) => {
