cd59aec28275847826ba5ae0bddd84d17c5d6512
Gilsabo <gilsala_88@hotmail.com>
2024-02-07 09:31:54 +0100

    Change format

--- migrations/00002-createTableUserProfiles.ts
+++ migrations/00002-createTableUserProfiles.ts
@@ -15,24 +15,22 @@ export type UserProfile = {
 
 export async function up(sql: Sql) {
   await sql`
-  CREATE TABLE user_profiles(
-    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    first_name varchar(30) NOT NULL,
-    last_name varchar(30) NOT NULL,
-    email varchar(30) NOT NULL UNIQUE,
-    age integer NOT NULL,
-    years_experience integer NOT NULL,
-    dominant_hand varchar(30) NOT NULL,
-    description varchar(250) NOT NULL,
-    profile_picture_url varchar(250),
-    user_id integer NOT NULL REFERENCES users (id) ON DELETE CASCADE
-  )
-
+    CREATE TABLE
+      user_profiles (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        first_name VARCHAR(30) NOT NULL,
+        last_name VARCHAR(30) NOT NULL,
+        email VARCHAR(30) NOT NULL UNIQUE,
+        age INTEGER NOT NULL,
+        years_experience INTEGER NOT NULL,
+        dominant_hand VARCHAR(30) NOT NULL,
+        description VARCHAR(250) NOT NULL,
+        profile_picture_url VARCHAR(250),
+        user_id INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE
+      )
   `;
 }
 
 export async function down(sql: Sql) {
-  await sql`
-  DROP TABLE users
-`;
+  await sql` DROP TABLE users `;
 }
