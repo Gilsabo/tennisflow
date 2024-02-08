4780eca859fde6768f94d21190f20651a63b18eb
Gilsabo <gilsala_88@hotmail.com>
2024-02-08 11:51:57 +0100

    Add alert when delete

--- app/dashboard/my-videos/DeleteVideoButton.tsx
+++ app/dashboard/my-videos/DeleteVideoButton.tsx
@@ -11,7 +11,7 @@ export default function DeleteVideoButton() {
         );
 
         if (confirmed) {
-          router.push('/');
+          alert('The video was succesfully deleted');
         } else {
           console.log('Deletion canceled');
         }
