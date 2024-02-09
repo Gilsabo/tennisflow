c0f41c6c25906a5beb3705527b00f43d46f56328
Gilsabo <gilsala_88@hotmail.com>
2024-02-09 11:40:53 +0100

    Create endpoint deleteVideoId

---
+++ app/api/deletevideos/[deletevideoVideoId]/route.ts
@@ -0,0 +1,53 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { z } from 'zod';
+import { deleteVideo } from '../../../../database/videos';
+import { Video } from '../../../../migrations/00003-createTableVideos';
+
+export type Error = {
+  error: string;
+};
+
+export type videoResponseBodyDelete =
+  | {
+      video: Video;
+    }
+  | Error;
+
+const userSchema = z.object({
+  videoId: z.number(),
+  videoUrl: z.string(),
+});
+
+export async function DELETE(
+  request: NextRequest,
+): Promise<NextResponse<videoResponseBodyDelete>> {
+  const body = await request.json();
+
+  const result = userSchema.safeParse(body);
+
+  if (!result.success) {
+    // zod send you details about the error
+
+    return NextResponse.json(
+      {
+        error: 'video does not exist',
+      },
+      { status: 400 },
+    );
+  }
+
+  const deletedVideo = await deleteVideo(result.data.videoId);
+
+  if (!deletedVideo) {
+    return NextResponse.json(
+      {
+        error: 'Error deleting Video',
+      },
+      { status: 500 },
+    );
+  }
+
+  return NextResponse.json({
+    video: deletedVideo,
+  });
+}
