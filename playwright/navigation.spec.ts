3a7165726b01d89425b92072221947ac48f0f5ab
Gilsabo <gilsala_88@hotmail.com>
2024-02-12 19:29:37 +0100

    Remove logalshost

--- playwright/navigation.spec.ts
+++ playwright/navigation.spec.ts
@@ -1,7 +1,7 @@
 import test, { expect } from '@playwright/test';
 
 test('navigation login', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+  await page.goto('/');
 
   await expect(page).toHaveTitle(/Tennisflow | all about tennis/);
 
