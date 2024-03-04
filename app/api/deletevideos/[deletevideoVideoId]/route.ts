6c37be5ba671bb1f339bdbe4eb75f6a033e8d912
Gilsabo <gilsala_88@hotmail.com>
2024-03-04 12:00:19 +0100

    Add method to delete video from Cloudinary

--- app/api/deletevideos/[deletevideoVideoId]/route.ts
+++ app/api/deletevideos/[deletevideoVideoId]/route.ts
@@ -1,7 +1,18 @@
+import { v2 as cloudinary } from 'cloudinary';
+import dotenv from 'dotenv';
 import { NextRequest, NextResponse } from 'next/server';
 import { z } from 'zod';
 import { deleteVideo } from '../../../../database/videos';
 import { Video } from '../../../../migrations/00003-createTableVideos';
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
 
 export type Error = {
   error: string;
@@ -35,6 +46,13 @@ export async function DELETE(
       { status: 400 },
     );
   }
+
+  cloudinary.api
+    .delete_resources([`${result.data.videoUrl}`], {
+      type: 'upload',
+      resource_type: 'video',
+    })
+    .then(console.log);
 
   const deletedVideo = await deleteVideo(result.data.videoId);
 
