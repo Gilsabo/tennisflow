3ceb7d2bedd6fc4c606954d9a9dc01bf11837b4f
Gilsabo <gilsala_88@hotmail.com>
2024-03-09 16:49:09 +0100

    Remove unnecessary comment

--- app/api/(auth)/register/route.ts
+++ app/api/(auth)/register/route.ts
@@ -72,18 +72,6 @@ export async function POST(
     );
   }
 
-  // 6. Send the new cookie in the headers
-
-  // cookies().set({
-  //   name: 'sessionToken',
-  //   value: session.token,
-  //   httpOnly: true,
-  //   path: '/',
-  //   secure: process.env.NODE_ENV === 'production',
-  //   maxAge: 60 * 60 * 48, // Expires in 24 hours,
-  //   sameSite: 'lax', // this prevents CSRF attacks
-  // });
-
   cookies().set({
     name: 'sessionToken',
     value: session.token,
