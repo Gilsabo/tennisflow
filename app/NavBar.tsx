e36cebbae588fbbf59f1fec1fde6b48248f6fc2e
Gilsabo <gilsala_88@hotmail.com>
2024-09-22 09:48:20 +0200

    Add underline styl to navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -22,7 +22,12 @@ export default function NavBar() {
             </Link>
           </li>
           <li className="ml-8 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-75">
-            <Link href="/dashboard">Dashboard</Link>
+            <Link
+              href="/dashboard"
+              className="relative inline-block text-white before:absolute before:bottom-0 before:left-1/2 before:h-0.5 before:w-0 before:bg-white before:transition-all before:duration-500 before:ease-in-out hover:before:w-full hover:before:left-0"
+            >
+              Dashboard
+            </Link>
           </li>
           <li className="ml-8 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-75">
             <a href="#community">Community</a>
