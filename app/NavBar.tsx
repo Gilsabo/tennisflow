12193780d83dfad1f6d0150d5d051583b25c7d2b
Gilsabo <gilsala_88@hotmail.com>
2024-10-03 11:18:02 +0200

    Add style to condicional navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -110,7 +110,12 @@ export default function NavBar() {
               </Link>
             </li>
             <li className="ml-8 hover:text-[#00503C] hover:bg-slate-200 rounded pl-1">
-              <a href="/#community">Community</a>
+              <a
+                href="/#community"
+                className="relative inline-block text-white before:absolute before:bottom-0 before:left-1/2 before:h-0.5 before:w-0 before:bg-white before:transition-all before:duration-500 before:ease-in-out hover:before:w-full hover:before:left-0"
+              >
+                Community
+              </a>
             </li>
             <li className="ml-8 hover:text-[#00503C] hover:bg-slate-200 rounded pl-1">
               <a href="/#partners">Partners</a>
